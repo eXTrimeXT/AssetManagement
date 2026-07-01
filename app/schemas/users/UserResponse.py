6d1fe7b774f0bd3ac4c222d2d0ea1108b23636dd
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-01 11:46:52 +0300

    add list assets for UserResponse.py

--- app/schemas/users/UserResponse.py
+++ app/schemas/users/UserResponse.py
@@ -2,9 +2,6 @@ from pydantic import BaseModel, ConfigDict, EmailStr
 from datetime import datetime
 from typing import Optional, Dict, List
 
-from app.schemas.assets.AssetResponse import AssetResponseForUserResponse
-
-
 class UserBase(BaseModel):
     """Базовая схема пользователя"""
     user_tab_id: Optional[str] = None
@@ -36,6 +33,42 @@ class UserResponse(UserBase):
     updated_at: Optional[datetime] = None
     model_config = ConfigDict(from_attributes=True)
 
+class AssetForUserResponse(BaseModel):
+    """Краткая схема для списков"""
+    asset_id: int
+    name: str
+    inventory_id: str
+    serial_number: Optional[str]
+    asset_status: str
+    comment: Optional[str] = None
+
+    model_id: Optional[int] = None
+    model_name: Optional[str] = None
+
+    class_id: Optional[int] = None
+    class_name: Optional[str] = None
+
+    asset_type_id: Optional[int] = None
+    type_asset_en_name: Optional[str] = None
+    type_asset_name: Optional[str] = None
+
+    warehouse_id: Optional[int] = None
+    warehouse_name: Optional[str] = None
+
+    parent_id: Optional[int] = None
+    parent_name: Optional[str] = None
+
+    software_id: Optional[int] = None
+    software_office_type: Optional[str] = None
+
+    manufacturer_id: Optional[int] = None
+    manufacturer_name: Optional[str] = None
+
+    vendor_id: Optional[int] = None
+    vendor_name: Optional[str] = None
+
+    model_config = ConfigDict(from_attributes=True)
+
 class UserShortResponse(BaseModel):
     """Краткая схема для списков (без назначений)"""
     user_id: int
@@ -49,6 +82,6 @@ class UserShortResponse(BaseModel):
     email: Optional[EmailStr] = None
     permissions: Optional[Dict[str, Dict[str, bool]]] = {}
 
-    assets: List[AssetResponseForUserResponse] = []
+    assets: List[AssetForUserResponse] = []
 
     model_config = ConfigDict(from_attributes=True)

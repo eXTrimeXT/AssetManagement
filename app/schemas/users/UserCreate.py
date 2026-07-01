5f7b2b95dbdb270cfa6651bb305c8d1834a8113e
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-01 08:59:00 +0300

    add comment on UserCreate.py

--- app/schemas/users/UserCreate.py
+++ app/schemas/users/UserCreate.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, ConfigDict
-from typing import Optional
+from typing import Optional, Any
+
 
 class UserCreate(BaseModel):
     """Схема для создания нового пользователя"""
@@ -14,19 +15,12 @@ class UserCreate(BaseModel):
     email: Optional[EmailStr] = None        # Email (валидация формата)
     phone: Optional[str] = None             # Телефон
     is_active: bool = True                  # Статус по умолчанию
+    comment: Optional[Any] = None           # Комментарий
 
     model_config = ConfigDict(json_schema_extra={
         "example": {
             "user_tab_id": "12345",
             "owner": "Иванов Иван Иванович",
-            "user_en_name": "Ivanov Ivan",
-            "user_position": "Инженер",
-            "department_id": 3,
-            "division_id": 9,
-            "group_id": 13,
-            "email": "ivanov@company.com",
-            "phone": "+7 (999) 123-45-67",
-            "is_active": True,
-            "role": "user"
+            "comment": "Комментарий"
         }
     })

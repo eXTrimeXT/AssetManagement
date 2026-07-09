237a2a3f595191c304cf1cd7092f5832e61d8010
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-09 16:37:45 +0300

    РЕФАКТОРИНГ КОДА

--- app/services/auth/auth_service.py
+++ app/services/auth/auth_service.py
@@ -10,7 +10,6 @@ from app.models.zup.employee import Employee
 from app.database.connection import get_db
 from app.database.zup.crud_zup_employees import get_employee_by_login_or_email
 from app.services.zup.zup_integration import sync_all_data
-from models.zup import Employee
 
 logger = logging.getLogger(__name__)
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "")

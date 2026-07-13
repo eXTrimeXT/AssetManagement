9aebf07886b53657359c4e15a693d07d7df9b3ea
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-13 12:38:19 +0300

    change sync_all_data to sync_employee_data

--- app/routers/router_android_data.py
+++ app/routers/router_android_data.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from app.schemas.android_data.android_data_schemas import AndroidDataCreate, AndroidDataResponse
@@ -12,17 +12,23 @@ from app.database.crud_android_data import (
 )
 from app.middleware.LoggingMiddleware import logger
 from app.services.auth.auth_service import require_authorized_user
+from services.auth.permission_checker import check_permission
 
 router_android_data = APIRouter(prefix="/android-data", tags=["android_data"])
 
 @router_android_data.post("/", response_model=AndroidDataResponse, status_code=200)
 async def endpoint_create_android_data(
+        request: Request,
         data: AndroidDataCreate,
         db: AsyncSession = Depends(get_db),
         current_user = Depends(require_authorized_user)
 ):
-    # if not has_android_sender_permission(current_user):
-    #     raise HTTPException(status_code=403, detail="Доступ запрещен!")
+    has_perm = await check_permission(request, "android_data", "write")
+    if not has_perm:
+        raise HTTPException(
+            status_code=403,
+            detail=f"Нет права 'write' на тип актива 'android_data'"
+        )
     return await create_or_update_android_data(db, data)
 
 

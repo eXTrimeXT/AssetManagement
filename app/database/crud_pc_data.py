96cd05102ebec48ea4c083e079b9ed22c3e66b11
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-13 16:20:16 +0300

    repair TIME

--- app/database/crud_pc_data.py
+++ app/database/crud_pc_data.py
@@ -34,7 +34,7 @@ async def create_or_update_pc_data(db: AsyncSession, pc_data: PCDataCreate):
             setattr(db_pc, k, v)
         # db_pc.user_id = user_id  # Обновляем связь (может быть None)
         # db_pc.user_tab_id = user_tab_id  # Обновляем связь (может быть None)
-        db_pc.updated_at = datetime.now(timezone.utc)
+        db_pc.updated_at = datetime.now()
     else:
         db_pc = PCData(
             username=pc_data.user.username,
